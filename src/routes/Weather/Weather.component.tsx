bf5e98c33d73e61a0b2bc5da8b56464a7d0d70c8
Adsyarif <adrisansyarif@gmail.com>
2024-01-13 01:48:35 +0700

    Add and implement  Weather API

--- src/routes/Weather/Weather.component.tsx
+++ src/routes/Weather/Weather.component.tsx
@@ -21,7 +21,6 @@ const Weather = () => {
       const cityName = city.name;
       const cityWeatherList = list;
       setForcastDates(cityWeatherList);
-      console.log(cityWeatherList);
     } catch (error) {
       console.log(error);
     }
@@ -33,55 +32,83 @@ const Weather = () => {
   const handleBack = () => {
     navigate("/");
   };
+
+  console.log(forcastDates);
+  useEffect(() => {
+    const newCurrentDate = forcastDates?.filter((forecastDate: any) => {
+      const forecastDateTag = dayjs(forecastDate.dt * 1000)
+        .add(1, "day")
+        .format("DDMMYY");
+      return forecastDateTag === dayjs().format("DDMMYY");
+    });
+    // console.log(newCurrentDate);
+  }, []);
+
   if (token) {
     return (
-      <div className="max-w-2xl mx-auto py-6">
-        <button
-          onClick={handleBack}
-          type="button"
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >
-          Back
-        </button>
-        <div className="min-h-screen flex items-center justify-center">
-          <div className="flex flex-col bg-white rounded p-4 w-full max-w-xs">
-            <div className="font-bold text-xl">{cityName}</div>
-            {forcastDates?.map((forecastDate: any, id) => {
-              const forecastDateTag = dayjs(forecastDate.dt).format(
-                "dddd, D MMMM YYYY"
-              );
+      <>
+        <div className="max-w-2xl mx-auto py-6">
+          <button
+            onClick={handleBack}
+            type="button"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Back
+          </button>
+          <div className="font-bold text-xl flex flex-row justify-center flex-wrap gap-5 ">
+            {cityName}
+          </div>
+        </div>
 
-              return (
-                <>
-                  <div key={id} className="text-sm text-gray-500">
+        <div className="flex flex-row flex-wrap  justify-center gap-5 bg-white rounded p-4 ">
+          {forcastDates?.map((forecastDate: any, id) => {
+            const { dt, main, weather, wind } = forecastDate;
+            const forecastDateTag = dayjs(dt * 1000).format(
+              "dddd, D MMMM YYYY"
+            );
+            const forecastHour = dayjs(dt * 1000).format("HH:mm");
+            const { humidity, temp, temp_max, temp_min } = main;
+            const [contentWeather] = weather;
+            const { icon } = contentWeather;
+            const conclution = contentWeather.main;
+            console.log(conclution);
+            const { speed } = wind;
+            const celciusTempConverter = (temp: number) => {
+              return Math.ceil(temp - 273);
+            };
+
+            const tempCelcius = celciusTempConverter(temp);
+            const tempCelciusMax = celciusTempConverter(temp_max);
+            const tempCelciusMin = celciusTempConverter(temp_min);
+
+            return (
+              <>
+                <div
+                  key={id}
+                  className="basis-64 bg-white font-semibold text-center rounded-3xl border shadow-lg p-10 max-w-xs"
+                >
+                  <div className="text-sm text-gray-500 ">
                     {forecastDateTag}
                   </div>
+                  <div className="text-sm text-gray-500 ">
+                    {forecastHour} GMT+7
+                  </div>
                   <div className="mt-6 text-6xl self-center inline-flex items-center justify-center rounded-lg text-indigo-400 h-24 w-24">
-                    <svg
-                      className="w-32 h-32"
-                      fill="none"
-                      stroke="currentColor"
-                      viewBox="0 0 24 24"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M3 15a4 4 0 004 4h9a5 5 0 10-.1-9.999 5.002 5.002 0 10-9.78 2.096A4.001 4.001 0 003 15z"
-                      ></path>
-                    </svg>
+                    <img
+                      src={`https://openweathermap.org/img/wn/${icon}@2x.png`}
+                      alt=""
+                    />
                   </div>
                   <div className="flex flex-row items-center justify-center mt-6">
-                    <div className="font-medium text-6xl">24°</div>
+                    <div className="font-medium text-6xl">{tempCelcius}°</div>
                     <div className="flex flex-col items-center ml-6">
-                      <div>Cloudy</div>
+                      <div>{conclution}</div>
                       <div className="mt-1">
                         <span className="text-sm">
                           <i className="far fa-long-arrow-up"></i>
                         </span>
                         <span className="text-sm font-light text-gray-500">
-                          28°C
+                          {tempCelciusMax}°C
                         </span>
                       </div>
                       <div>
@@ -89,7 +116,7 @@ const Weather = () => {
                           <i className="far fa-long-arrow-down"></i>
                         </span>
                         <span className="text-sm font-light text-gray-500">
-                          20°C
+                          {tempCelciusMin}°C
                         </span>
                       </div>
                     </div>
@@ -97,23 +124,19 @@ const Weather = () => {
                   <div className="flex flex-row justify-between mt-6">
                     <div className="flex flex-col items-center">
                       <div className="font-medium text-sm">Wind</div>
-                      <div className="text-sm text-gray-500">9k/h</div>
+                      <div className="text-sm text-gray-500">{speed}k/h</div>
                     </div>
                     <div className="flex flex-col items-center">
                       <div className="font-medium text-sm">Humidity</div>
-                      <div className="text-sm text-gray-500">68%</div>
-                    </div>
-                    <div className="flex flex-col items-center">
-                      <div className="font-medium text-sm">Visibility</div>
-                      <div className="text-sm text-gray-500">10km</div>
+                      <div className="text-sm text-gray-500">{humidity}%</div>
                     </div>
                   </div>
-                </>
-              );
-            })}
-          </div>
+                </div>
+              </>
+            );
+          })}
         </div>
-      </div>
+      </>
     );
   }
   return <Navigate to="/" />;
